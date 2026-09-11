5676dcc6234612aa8a788f2a0f7fbb5218826505
stalvoro <alexander.vorobyev@stud-h.da.de>
2026-09-11 14:12:34 +0200

    inventory ui prototype added

--- scripts/UI_scripts/inventory_hud.gd
+++ scripts/UI_scripts/inventory_hud.gd
@@ -1,4 +1,197 @@
 extends Control
 
+@onready var exit_button: Button = $MarginContainer/VBoxContainer/MarginContainer/HBoxContainer/MarginContainer/exit_button
+@onready var item_button: Button = $MarginContainer/VBoxContainer/MarginContainer2/HBoxContainer/MarginContainer/VBoxContainer/item_button
+@onready var logbook_button: Button = $MarginContainer/VBoxContainer/MarginContainer2/HBoxContainer/MarginContainer/VBoxContainer/logbook_button
+@onready var skill_button: Button = $MarginContainer/VBoxContainer/MarginContainer2/HBoxContainer/MarginContainer/VBoxContainer/skill_button
+
+@onready var select_container: VBoxContainer = $MarginContainer/VBoxContainer/MarginContainer2/HBoxContainer/MarginContainer2/select_container
+@onready var info_container: RichTextLabel = $MarginContainer/VBoxContainer/MarginContainer2/HBoxContainer/MarginContainer3/info_container
+
+@onready var character_one_button: Button = $MarginContainer/VBoxContainer/MarginContainer3/VBoxContainer/MarginContainer/front_row_container/MarginContainer/character_one_button
+@onready var character_two_button: Button = $MarginContainer/VBoxContainer/MarginContainer3/VBoxContainer/MarginContainer/front_row_container/MarginContainer2/character_two_button
+@onready var character_three_button: Button = $MarginContainer/VBoxContainer/MarginContainer3/VBoxContainer/MarginContainer/front_row_container/MarginContainer3/character_three_button
+@onready var character_four_button: Button = $MarginContainer/VBoxContainer/MarginContainer3/VBoxContainer/MarginContainer2/back_row_container/MarginContainer/character_four_button
+@onready var character_five_button: Button = $MarginContainer/VBoxContainer/MarginContainer3/VBoxContainer/MarginContainer2/back_row_container/MarginContainer2/character_five_button
+
+
+@onready var character_button : PackedScene = preload("res://scenes/UI_scenes/ui_element_character_button.tscn")
+
+enum enum_button_neighbor {UP,DOWN,LEFT,RIGHT}
+
+var dummy_buttons_array : Array[String] = ["Button 1","Button 2","Button 3","Button 4"]
+
+var return_chain : Array[Button]
+var undo_containers : bool = false
+
+var current_button_focus : Button
+#---------------------------------------------------------------------------------
+var brunhilde_unit : unit = preload("res://resources/units/brunhilde_unit.tres")
+var casandra_unit : unit = preload("res://resources/units/casandra_unit.tres")
+var derek_uit : unit = preload("res://resources/units/derek_unit.tres")
+var karion_unit : unit = preload("res://resources/units/karion_unit.tres")
+var ungor_unit : unit = preload("res://resources/units/ungor_unit.tres")
+var small_healing_potion : item = preload("res://resources/items/small_healing_potion.tres")
+var med_kit : item = preload("res://resources/items/med_kit.tres")
+#---------------------------------------------------------------------------------
 func _ready() -> void:
 	ConsoleLog.SCENE(self,true)
+	#--------------------------------------------------------
+	TeamRoster.put_unit_into_combat_team(brunhilde_unit)
+	TeamRoster.put_unit_into_combat_team(casandra_unit)
+	TeamRoster.put_unit_into_combat_team(derek_uit)
+	TeamRoster.put_unit_into_combat_team(karion_unit)
+	TeamRoster.put_unit_into_combat_team(ungor_unit)
+	TeamRoster.put_item_into_inventory(small_healing_potion)
+	TeamRoster.put_item_into_inventory(med_kit)
+	#---------------------------------------------------------
+	current_button_focus = item_button
+	item_button.grab_focus.call_deferred()
+	return_chain.append(exit_button)
+	fill_character_container()
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("control_return"):
+		ConsoleLog.INPUT("control_return","is_action_pressed",[])
+		return_one_step()
+	if event.is_action_pressed("control_up"):
+		ConsoleLog.INPUT("control_up","is_action_pressed",[])
+		cycle_buttons_through_neighbors(enum_button_neighbor.UP)
+	if event.is_action_pressed("control_down"):
+		ConsoleLog.INPUT("control_down","is_action_pressed",[])
+		cycle_buttons_through_neighbors(enum_button_neighbor.DOWN)
+	if event.is_action_pressed("control_right"):
+		ConsoleLog.INPUT("control_down","is_action_pressed",[])
+		cycle_buttons_through_neighbors(enum_button_neighbor.RIGHT)
+	if event.is_action_pressed("control_left"):
+		ConsoleLog.INPUT("control_down","is_action_pressed",[])
+		cycle_buttons_through_neighbors(enum_button_neighbor.LEFT)
+	if event.is_action_pressed("control_confirm"):
+		ConsoleLog.INPUT("control_confirm","is_action_pressed",[])
+		confirm_button_input()
+
+func confirm_button_input():
+	current_button_focus.toggled.emit(true)
+	current_button_focus.pressed.emit()
+
+func return_one_step():
+	if return_chain.is_empty():
+		queue_free()
+	else:
+		current_button_focus = return_chain.pop_back()
+		current_button_focus.grab_focus.call_deferred()
+	
+	if undo_containers:
+		for cursor : Button in select_container.get_children():
+			cursor.queue_free()
+		info_container.text = ""
+		undo_containers = false
+
+func cycle_buttons_through_neighbors(button_neighbor : enum_button_neighbor):
+	var local_button : Button = null
+	var local_path : NodePath
+	
+	match button_neighbor:
+		enum_button_neighbor.UP:
+			if current_button_focus.focus_neighbor_top:
+				local_path = current_button_focus.focus_neighbor_top
+		enum_button_neighbor.DOWN:
+			if current_button_focus.focus_neighbor_bottom:
+				local_path = current_button_focus.focus_neighbor_bottom
+		enum_button_neighbor.LEFT:
+			if current_button_focus.focus_neighbor_left:
+				local_path = current_button_focus.focus_neighbor_left
+		enum_button_neighbor.RIGHT:
+			if current_button_focus.focus_neighbor_right:
+				local_path = current_button_focus.focus_neighbor_right
+	
+	if local_path:
+		local_button = current_button_focus.get_node(local_path)
+		local_button.grab_focus.call_deferred()
+		current_button_focus = local_button
+
+func fill_select_container(selected_array : Array):
+	var prev_button : Button = null
+	var is_first_button : bool = true
+	var first_button : Button = null
+	var is_last_button : bool = false
+	
+	for cursor in selected_array:
+	
+		if cursor == selected_array.back():
+			is_last_button = true
+		
+		var new_button : Button = Button.new()
+		if cursor is quest:
+			new_button.text = cursor.quest_name
+			new_button.focus_entered.connect(_fill_info_container.bind(cursor.quest_description))
+		
+		if cursor is item:
+			new_button.text = cursor.item_name
+			new_button.focus_entered.connect(_fill_info_container.bind(cursor.item_description))
+		
+		select_container.add_child(new_button)
+		ConsoleLog.INFO(self,["new_button","text","path"],[new_button,cursor,new_button.get_path()])
+		if is_first_button:
+			is_first_button = false
+			first_button = new_button
+			current_button_focus = new_button
+			current_button_focus.grab_focus.call_deferred()
+		else:
+			new_button.set_focus_neighbor(SIDE_TOP,prev_button.get_path())
+			
+			if is_last_button:
+				new_button.set_focus_neighbor(SIDE_BOTTOM, first_button.get_path())
+				first_button.set_focus_neighbor(SIDE_TOP, new_button.get_path())
+			
+			prev_button.set_focus_neighbor(SIDE_BOTTOM, new_button.get_path())
+		prev_button = new_button
+
+func fill_character_container():
+	if TeamRoster.combat_team[0]:
+		var new_button : character_button_ui = character_button.instantiate()
+		new_button.make_character_ui_element(TeamRoster.combat_team[0])
+		character_one_button.add_child.call_deferred(new_button)
+	if TeamRoster.combat_team[1]:
+		var new_button : character_button_ui = character_button.instantiate()
+		new_button.make_character_ui_element(TeamRoster.combat_team[1])
+		character_two_button.add_child.call_deferred(new_button)
+	if TeamRoster.combat_team[2]:
+		var new_button : character_button_ui = character_button.instantiate()
+		new_button.make_character_ui_element(TeamRoster.combat_team[2])
+		character_three_button.add_child.call_deferred(new_button)
+	if TeamRoster.combat_team[3]:
+		var new_button : character_button_ui = character_button.instantiate()
+		new_button.make_character_ui_element(TeamRoster.combat_team[3])
+		character_four_button.add_child.call_deferred(new_button)
+	if TeamRoster.combat_team[4]:
+		var new_button : character_button_ui = character_button.instantiate()
+		new_button.make_character_ui_element(TeamRoster.combat_team[4])
+		character_five_button.add_child.call_deferred(new_button)
+
+func select_unit_roster():
+	current_button_focus = character_one_button
+	character_one_button.grab_focus.call_deferred()
+
+func _fill_info_container(description : String):
+	info_container.text = description
+
+func _on_item_button_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		undo_containers = true
+		return_chain.append(item_button)
+		fill_select_container(TeamRoster.consumables)
+
+func _on_logbook_button_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		undo_containers = true
+		return_chain.append(logbook_button)
+		fill_select_container(TeamRoster.logbook)
+
+func _on_exit_button_pressed() -> void:
+	queue_free()
+
+func _on_skill_button_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		return_chain.append(skill_button)
+		select_unit_roster()
--- scripts/resource_scripts/unit.gd
+++ scripts/resource_scripts/unit.gd
@@ -5,6 +5,7 @@ class_name unit
 @export var unit_sprite_path : String
 @export var unit_name : String
 @export var unit_level : int
+var unit_skillpoints : int
 
 @export var attributes : Dictionary[String,int] = {
 	"strength":0,
